be1835b96486980c310d24b09b5016714e73cf47
ajay123938 <ajay.ag593@gmail.com>
2025-06-17 14:13:27 +0545

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -15,7 +15,6 @@ import hmac
 # Create your views here.
 def home(request):
        all_product = Product.objects.all()
-       all_product=  all_product.filter(name__icontains = request.POST.get('search'))
        context = {'objs':all_product}
        return render(request,'index.html',context)
 
